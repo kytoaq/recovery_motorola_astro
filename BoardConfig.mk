726ad9f22db09a1c437a2b3f5ca51c84478ef4c7
Joshua Blanchard <joshua.lee.bbg@gmail.com>
2021-03-27 16:15:32 -0600

    astro: Set TARGET_USE_CUSTOM_LUN_FILE_PATH
    Signed-off-by: Joshua Blanchard <joshua.lee.bbg@gmail.com>
    Change-Id: I21b157da329a11d58260d6e74854ed88e718fa31

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -140,4 +140,5 @@ TW_USE_TOOLBOX := true
 TW_Y_OFFSET := 80
 TW_H_OFFSET := -80
 
-
+# Team Win Recovery - USB
+TARGET_USE_CUSTOM_LUN_FILE_PATH := /config/usb_gadget/g1/functions/mass_storage.0/lun.%d/file
