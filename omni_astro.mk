2883ec39e4822f454a8ca2877954ff1d0c5e5c13
kytoaq <kytoaq@gmail.com>
2021-03-27 00:27:14 -0600

    astro: Add device.mk
    Delete some revisions in omni_astro.mk > Move to device.mk
    
    Signed-off-by: kytoaq <kytoaq@gmail.com>

--- omni_astro.mk
+++ omni_astro.mk
@@ -24,40 +24,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
 # Inherit some common Omni stuff.
 $(call inherit-product, vendor/omni/config/common.mk)
 
-# A/B
-AB_OTA_PARTITIONS += \
-    boot \
-    dtbo \
-    system \
-    vendor \
-    vbmeta
-
-AB_OTA_POSTINSTALL_CONFIG += \
-    RUN_POSTINSTALL_system=true \
-    POSTINSTALL_PATH_system=system/bin/otapreopt_script \
-    FILESYSTEM_TYPE_system=ext4 \
-    POSTINSTALL_OPTIONAL_system=true
-
-PRODUCT_PACKAGES += \
-    otapreopt_script \
-    update_engine \
-    update_verifier \
-    update_engine_sideload
-
-# Boot control HAL
-PRODUCT_PACKAGES += \
-    android.hardware.boot@1.0-impl \
-    android.hardware.boot@1.0-service
-
-PRODUCT_PACKAGES += \
-    bootctrl.sdm710
-
-PRODUCT_STATIC_BOOT_CONTROL_HAL := \
-    bootctrl.sdm710 \
-    libcutils \
-    libgptutils \
-    libz
-
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := astro
 PRODUCT_NAME := omni_astro
